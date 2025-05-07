27ae1223d4f3e3666d0fff09e7d9e961eb4aa33f
Kevin Ernest Long <kevinelong@gmail.com>
2025-05-07 10:46:21 -0700

    update

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,59 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String example1 = """
+                #
+                ##
+                ###
+                ####
+                """;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("'" + example1 + "'");
+
+        String result = getPyramid(4);
+        System.out.println("'" + result + "'");
+
+        assert result.trim().equals(example1.trim()); //ignore whitespace on each end
+
+        //another test, but with not output
+        assert getPyramid(2).trim().equals("""
+                #
+                ##
+                """.trim()); //ignore whitespace on each end
+        assert getPyramid(3).trim().equals("""
+                #
+                ##
+                ###
+                """.trim()); //ignore whitespace on each end
+
+    }
+    static String getPyramid(int n){
+        String output = "";
+        for(int i=0; i < n; i++){
+            for(int j = 0; j < (i + 1); j++){
+                output += "#";
+            }
+            output += "\n";
         }
+        return output;
+        //lines 1,2,3
+        //quantity chars follows line number - going up to it
     }
 }
+/*
+LEVEL 2:
+   #
+  ##
+ ###
+####
+LEVEL 3:
+   #
+  ###
+ #####
+#######
+LEVEL 4:
+   # #
+  ## ##
+ ### ###
+#### ####
+ */
